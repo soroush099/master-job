d3a98d13c1faff916bcbfe03b8915ab2978ec20d
soroush099 <141868381+soroush099@users.noreply.github.com>
2024-09-15 16:00:06 +0330

    created model: CustomUser in app: profiles/models.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,12 @@
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    pass
 
 
 class Address(models.Model):
