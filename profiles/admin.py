d94c49bfce0d500de3f10ee00a1ff12d7aa14dea
soroush099 <141868381+soroush099@users.noreply.github.com>
2024-09-16 11:42:12 +0330

    created admin.py in app: profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from profiles.models import *
+
+
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    pass
