0d7412abe49e8a998b49dce28939ebb9d6f35642
soroush099 <141868381+soroush099@users.noreply.github.com>
2024-09-17 11:23:20 +0330

    created profile views,serializers,urls in app: profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from profiles.views import *
+
 urlpatterns = [
+    path("register/", CreateUserView.as_view()),
+    path("profile/", ProfileView.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from profiles.models import CustomUser
+from profiles.serializers import CustomUserSerializer, PutUserSerializer
+
 
 # Create your views here.
+
+class CreateUserView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+            token, created = Token.objects.get_or_create(user=user)
+            response_data = {
+                'user_id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'token': token.key,
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileView(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get(self, request):
+        user = request.user
+        query = CustomUser.objects.get(pk=user.id)
+        serializer = CustomUserSerializer(query)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        query = CustomUser.objects.get(pk=user.id)
+        serializer = PutUserSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
