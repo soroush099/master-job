00eb536e9a910fdf32190738e1515e9f05c89387
soroush099 <141868381+soroush099@users.noreply.github.com>
2024-12-09 02:41:04 +0330

    Delete file querys.py

--- packages/querys.py
+++
@@ -1,5 +0,0 @@
-def get_query_by_user_id(request, serializer_name, model_name, *, many_bool):
-    user = request.user
-    query = model_name.objects.filter(user_id=user.id)
-    serializer = serializer_name(query, many=many_bool)
-    return serializer.data
